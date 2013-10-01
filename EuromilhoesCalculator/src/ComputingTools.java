86851afe538125c2a3af5597e4e89ebab85effc4
Rui Romão <rehaellspam@gmail.com>
2013-10-01 01:02:28 +0100

    actualização

---
+++ EuromilhoesCalculator/src/ComputingTools.java
@@ -0,0 +1,56 @@
+
+import java.util.*;
+
+public class ComputingTools {
+
+
+	public int[] random(int arrayLength,int minimo, int maximo) {
+		int[] arrayNum = new int[arrayLength];
+		boolean numRepetido = false;
+
+		//main cycle that runs arrayLength - 1 times
+		for(int i = 0; i < arrayNum.length; i++){
+			int randNum = minimo + (int)(Math.random() * maximo );
+			//System.out.print("O numero gerado foi : " + randNum + "\n");
+
+
+			while(true){
+				//checks if randNum is present on the arrayNum, toggles a flag if so.
+				for(int j = 0; j < arrayNum.length ; j++){
+					if(arrayNum[j] == randNum){
+						numRepetido = true;
+						break;
+					} else 
+						numRepetido = false;
+				}
+				//if randNum is repeated in the array, randomizes new randNum
+				if(numRepetido == true){
+					randNum = minimo + 	(int)(Math.random() * maximo );
+				//	System.out.print("O numero gerado foi repetido, gerado um novo  : " + randNum + "\n");
+				} else{
+					//if not, adds it to the array
+					arrayNum[i] = randNum;
+					//System.out.print("O numero foi escrito no Array : " + arrayNum[i] + "\n");
+					break;
+				}
+			}					
+		}
+
+		//sorts the final array with no repetitions for nicer look 
+		Arrays.sort(arrayNum);
+		return arrayNum;
+
+
+	}
+
+	//if maximum random number not specified assume 100
+	public int[] random(int arrayLength) {
+		return this.random(arrayLength,100);
+	}
+
+	//if minimum random number not specified assume 1
+	public int[] random(int arrayLength, int maximo) {
+		return this.random(arrayLength, 1, maximo);
+	}
+
+}
---
+++ EuromilhoesCalculator/src/EuroMilhoes.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class EuroMilhoes {
+
+
+	public static void main(String[] args) {
+		//User numbers
+		int[] userNum = { 5, 10, 14, 25, 45 };
+		int[] userEst = {8, 11};
+
+		Arrays.sort(userNum);
+		Arrays.sort(userEst);
+
+		//Random picked numbers
+		ComputingTools arr = new ComputingTools();
+		int[] randNum = arr.random(5,50);
+		int[] randEst = arr.random(2,11);
+
+		Vector prizeNum = new Vector();
+		Vector prizeEst = new Vector();
+
+		System.out.println("\nNumeros do jogador");
+		for(int i = 0; i < userNum.length; i++)
+			System.out.print(userNum[i] + " ");
+
+		System.out.println("\nEstrelas do jogador");
+		for(int i = 0; i < userEst.length; i++)
+			System.out.print(userEst[i] + " ");
+
+		System.out.println("\nNumeros sorteados");
+		for(int i = 0; i < randNum.length; i++){
+			System.out.print(randNum[i] + " ");
+		}
+		System.out.println("\nEstrelas sorteados");
+		for(int i = 0; i < randEst.length; i++){
+			System.out.print(randEst[i] + " ");
+		}
+
+		for(int i = 0; i < userNum.length; i++)
+			for(int j = 0; j < userNum.length; j++)
+				if(randNum[i] == userNum[j]){
+					prizeNum.add(userNum[j]);
+				}
+
+		for(int i = 0; i < userEst.length; i++)
+			for(int j = 0; j < userEst.length; j++)
+				if(randEst[i] == userEst[j]){
+					prizeEst.add(userEst[j]);
+				}
+
+		if(prizeNum.size() != 0 || prizeEst.size() != 0 ){
+			System.out.println("\n\nNumeros premiados : " + prizeNum);
+			System.out.println("\n\nEstrelas premiadas : " + prizeEst);
+		}
+		else
+			System.out.println("\n\nNão acertou nenhum numero ou estrela!");
+
+	}
+
+}
